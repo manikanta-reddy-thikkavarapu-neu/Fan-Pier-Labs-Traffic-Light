978983b0305c7699a2b9cdd16b05d2907207b40b
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2024-07-03 14:20:27 -0400

    Traffic Light Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [trigger, setTrigger] = useState(false);
+
+  useEffect(() => {
+    const redLight = document.getElementById("red");
+    redLight.style.backgroundColor = "red";
+
+    const greenLight = document.getElementById("green");
+
+    const yellowLight = document.getElementById("yellow");
+
+    setTimeout(() => {
+      redLight.style.backgroundColor = "white";
+      greenLight.style.backgroundColor = "green";
+    }, 3000);
+
+    setTimeout(() => {
+      greenLight.style.backgroundColor = "white";
+      yellowLight.style.backgroundColor = "yellow";
+    }, 5000);
+
+    setTimeout(() => {
+      yellowLight.style.backgroundColor = "white";
+      redLight.style.backgroundColor = "red";
+      setTrigger(!trigger);
+    }, 6000);
+  }, [trigger]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="red" class="lights"></div>
+      <div id="yellow" class="lights"></div>
+      <div id="green" class="lights"></div>
+    </>
   );
 }
 
