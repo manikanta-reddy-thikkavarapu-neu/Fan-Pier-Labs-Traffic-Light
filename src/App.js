469bbb8d361625a65d9d5ea388e393918bb5a3b6
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2024-10-16 20:55:17 -0400

    Multiple Approached

--- src/App.js
+++ src/App.js
@@ -39,3 +39,89 @@ function App() {
 }
 
 export default App;
+
+// Below code no need to use document object:
+
+// import { useEffect, useState } from "react";
+// import "./App.css";
+
+// function App() {
+//   const [currentLight, setCurrentLight] = useState("red");
+
+//   useEffect(() => {
+//     const cycleLights = () => {
+//       if (currentLight === "red") {
+//         setTimeout(() => setCurrentLight("green"), 3000);
+//       } else if (currentLight === "green") {
+//         setTimeout(() => setCurrentLight("yellow"), 2000);
+//       } else if (currentLight === "yellow") {
+//         setTimeout(() => setCurrentLight("red"), 1000);
+//       }
+//     };
+
+//     cycleLights();
+//   }, [currentLight]);
+
+//   return (
+//     <div className="traffic-light">
+//       <div
+//         className={`light ${currentLight === "red" ? "red" : ""}`}
+//       ></div>
+//       <div
+//         className={`light ${currentLight === "yellow" ? "yellow" : ""}`}
+//       ></div>
+//       <div
+//         className={`light ${currentLight === "green" ? "green" : ""}`}
+//       ></div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// Below code is using useRef instead of document object:
+
+// import { useEffect, useRef, useState } from "react";
+// import "./App.css";
+
+// function App() {
+//   const redRef = useRef(null);
+//   const yellowRef = useRef(null);
+//   const greenRef = useRef(null);
+
+//   const [trigger, setTrigger] = useState(false);
+
+//   useEffect(() => {
+//     const redLight = redRef.current;
+//     const greenLight = greenRef.current;
+//     const yellowLight = yellowRef.current;
+
+//     redLight.style.backgroundColor = "red";
+
+//     setTimeout(() => {
+//       redLight.style.backgroundColor = "white";
+//       greenLight.style.backgroundColor = "green";
+//     }, 3000);
+
+//     setTimeout(() => {
+//       greenLight.style.backgroundColor = "white";
+//       yellowLight.style.backgroundColor = "yellow";
+//     }, 5000);
+
+//     setTimeout(() => {
+//       yellowLight.style.backgroundColor = "white";
+//       redLight.style.backgroundColor = "red";
+//       setTrigger(!trigger);
+//     }, 6000);
+//   }, [trigger]);
+
+//   return (
+//     <>
+//       <div ref={redRef} className="lights"></div>
+//       <div ref={yellowRef} className="lights"></div>
+//       <div ref={greenRef} className="lights"></div>
+//     </>
+//   );
+// }
+
+// export default App;
